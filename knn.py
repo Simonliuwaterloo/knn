f412c7fc0bf8bf3a1218992ba7b8424e07c5c9f2
Simon Li <simonli1215@outlook.com>
2018-09-15 19:33:59 -0400

    add load_data module to load data from csv files

--- knn.py
+++ knn.py
@@ -1,9 +1,27 @@
 import numpy as np
+import pandas as pd
 from math import sqrt
 
-def data():
+def data_generate():
     location = np.array([[1.0, 0.9], [1.1, 1.3], [0.1, 0.2], [0.0, 0.1]])
     classes = ['A', 'A', 'B', 'B']
+    return location, classes
+
+def load_data():
+    data_init = pd.read_csv("test_data.txt")
+    #read data
+    array_init = np.array(data_init)
+    #convert data type from data frame to array
+    location = np.zeros(shape = (b.shape[0],b.shape[1]-1))
+    #creat empty array in which we put coordinates of data
+    for n in range(0,b.shape[0]):
+        for m in range(0,useful.shape[1]):
+            location[n,m]=b[n,m]
+    #fill coordinates of data
+    classes = list()
+    #do the same thing for classes
+    for n in range(0,b.shape[0]):
+        classes.append(array_init[n,2])
     return location, classes
 
 def classify(input, location, classes, k):
