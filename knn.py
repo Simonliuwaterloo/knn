f2d91ed76a2c5df1b72216547893438aac365543
Simon Li <simonli1215@outlook.com>
2018-09-15 21:27:21 -0400

    add plotting, fix bugs

--- knn.py
+++ knn.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
 import numpy as np
 import pandas as pd
 from math import sqrt
+import matplotlib.pyplot as plt
+import tkinter
 
 def data_generate():
     location = np.array([[1.0, 0.9], [1.1, 1.3], [0.1, 0.2], [0.0, 0.1]])
@@ -12,17 +15,27 @@ def load_data():
     #read data
     array_init = np.array(data_init)
     #convert data type from data frame to array
-    location = np.zeros(shape = (b.shape[0],b.shape[1]-1))
+    location = np.zeros(shape = (array_init.shape[0],array_init.shape[1]-1))
     #creat empty array in which we put coordinates of data
-    for n in range(0,b.shape[0]):
-        for m in range(0,useful.shape[1]):
-            location[n,m]=b[n,m]
+    for n in range(0,array_init.shape[0]):
+        for m in range(0,location.shape[1]):
+            location[n,m]=array_init[n,m]
     #fill coordinates of data
     classes = list()
     #do the same thing for classes
-    for n in range(0,b.shape[0]):
+    for n in range(0,array_init.shape[0]):
         classes.append(array_init[n,2])
     return location, classes
+
+def plot_data(location, classes):
+    x=list()
+    y=list()
+    for n in range(location.shape[0]):
+        x.append(location[n,0])
+        y.append(location[n,1])
+    plt.scatter(x,y)
+    plt.show()
+    return 0
 
 def classify(input, location, classes, k):
     #get x and y value of input
@@ -48,3 +61,7 @@ def classify(input, location, classes, k):
         print('A')
     else:
         print('B')
+
+
+a,b = load_data()
+plot_data(a,b)
